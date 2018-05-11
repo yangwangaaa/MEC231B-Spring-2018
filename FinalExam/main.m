8a8b03ebf96bc1805494e3a81890703f28a38813
Jun Zeng <zengjunsjtu@berkeley.edu>
2018-05-10 21:27:57 -0700

    Final

--- FinalExam/main.m
+++ FinalExam/main.m
@@ -22,9 +22,9 @@ q1e;
 %% Robust Control
 
 %% 2
-% In the line 3, we use norm calculator with a setting of accuracy to get
+% At the line 3, we use norm calculator with a setting of accuracy to get
 % the maximum frequency response and its corresponded frequency, thus in
-% the line 4, we have A = abs(freqresp(G,B)). In the line 5, we use frd to
+% the line 4, we have A = abs(freqresp(G,B)). At the line 5, we use frd to
 % compare the system G with a newly defined system, where at the frequency B,
 % the frequency response is exactly the maximum frequency response of G.
 % Finally, we see the superposition of bodegram of two these system at
@@ -33,6 +33,13 @@ q1e;
 q2;
 
 %% 3
-% 
-
+% At the line 4, we use random complex number delta to generate D. We go to
+% the file cnum2sys.m, as we have seen in the homework and the slide, the
+% behavior of cnumsys.m is that the infinity norm of this system is the
+% same as the complex number generated and the associated frequency is
+% wBar.  At the line 5, we calculate the frequency 
+% response of D and to compare it with delta (absolutely the same!). At the
+% line 6, we verify that wBar is indeed the frequency. At the line 7, we
+% generate the bode plot of D, we find out the gain is constant and equals
+% to 20*log(delta).
 q3;
